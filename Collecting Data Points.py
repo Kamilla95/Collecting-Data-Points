fd135248f2a2585c28ed9cda185e29906a8b5c68
Kamilla <65569451+Kamilla95@users.noreply.github.com>
2022-06-15 13:48:08 +0300

    Add files via upload

---
+++ Collecting Data Points.py
@@ -0,0 +1,8 @@
+minimum_points = int(input())
+data_points = int(input())
+
+if data_points >= minimum_points:
+    print('There are enough data points!')
+if data_points < minimum_points:
+    print('Keep collecting data.')
+
